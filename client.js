06504d7b2c0a83601c2af1d622b922acd95ff00a
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-11-16 10:54:07 -0600

    added sockets.io to alert user when another user connects

--- client.js
+++ client.js
@@ -1,10 +1,19 @@
+/*initializing socket.io
+  initialization tries to make connection to Socket.io server at the same URL that the current page is being hosted on. 
+  a callback function needs to be set up for the connection event that alerts the user whenever a new user connects. This is done in the server.js file. */
+var socket = io();
 
 $(() => {
 
       $("#send").click(() => {
-        addMessages({ name: "Tim", message: "Hello" });
-      })
-  })
+        var message = {
+            name: $("#name").val(),
+            message: $("#message").val()
+        };
+        postMessages(message);
+        })
+      getMessages();
+    });
 
 // add messages function
 //It takes a parameter of a message object
@@ -12,4 +21,18 @@ $(() => {
 function addMessages(message) {
     $("#messages").append(`<h4> ${message.name} </h4> 
           <p> ${message.message} </p>`);
-};
+  };
+
+  /*This function will get the messages from the server.js file and post    all previous (not new messages) messages on the DOM.
+    This is an AJAX request. 
+    The callback function in the .get request has a callback function that takes in data that is being fetched as a parameter. */
+  function getMessages() { 
+      $.get('http://localhost:3000/messages', (data) => {
+          //This populeate the DOM with the message history in a for each loop. 
+          data.forEach(addMessages);
+          });
+      };
+  function postMessages(message) { 
+      $.post('http://localhost:3000/messages', message)
+       };
+  
--- server.js
+++ server.js
@@ -2,12 +2,26 @@
 //always require express if using it.
 var express = require('express');
 
+//add body-parser
+var bodyParser = require('body-parser');
+
 //define a variable, app, that is an instance of express:
 var app = express();
 
-//chaining will be used as the express app is told what to do.
+/*http server set up required for socket.io.
+  The http library from node is required.  
+  .Server is called on the require('http') with the express app is passed in as a parameter.  */
+var http = require('http').Server(app);
 
-//.use is another method of the express object that server static files.
+//sockets.io must be required to be used.
+//reference to http must be part of the io variable.
+//socket.io must also be set-up on the front-end.
+var io = require('socket.io')(http);
+
+//chaining will be used to tell the express app what to do. 
+
+// ********* app.use ******** //
+//.use is another method of the express object that serves static files.
 //the first parameter is expresss.static(), which is really a function.
 //(__dirname) is passed into the express.static() function.
 //express.static is a built-in middleware function of express.
@@ -16,15 +30,52 @@ var app = express();
 //The file served is:req.url + root directory.
 //The options object has several different choices.  
 app.use(express.static(__dirname));
-//If the server is started again, hello will be in the broswer.
 
-//This code tells the express app to listen for requests on port 3000.
-/*.listen is a method of the express object that takes a port as the first parameter.
-  3000 is the port where this app will run. 
-  app.listen takes a callback function as the 2nd parameter.
-  The callback function is anonymous so, takes no parameters. 
-  The port should be a variable because it could change once the app is deployed.  
-  It shouldn't be hardcoded. 
+//body-parser needs to be set up with middleware so it can be used.
+//This tells bodyParser that JSON will be coming in with http requests. 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var messages = [
+      {
+        name: "Tim",
+        message: "Hi"
+      },
+      {
+        name: "Sarah",
+        message: "Hello"
+      }
+  ];
+
+// ********* app.get ********* //
+//This specifies that the server is handling "get" requests.
+//app.get is a method of the express object.
+//The first parameter is the route being used for the get request.
+//The second parameter is a callback function to handle the request.
+//The callback function takes in two parameters of req and res.
+app.get('/messages', (req,res) => {
+  res.send(messages); //the variable declared at the top of the file. 
+});
+
+app.post('/messages', (req,res) => {
+  console.log("This is the req.body: ", req.body);
+  messages.push(req.body);
+  res.sendStatus(200);
+});
+
+/*This is the callback function that checks for connections   of other users.
+  The function takes in a socket and has an anonymous callback function. */
+io.on('connection', (socket) => {
+    console.log("A user connected on: ", socket);
+});
+
+// ********** app.listen ********** //
+/* This code tells the express app to listen for requests on a            specific port.
+  .listen is a method of the express object that has two parameters.
+  The 1st parameter is the port number.
+  The 2nd parameter is a callback function.
+  The callback function is anonymous so,  it takes no parameters. 
+  The port should be a variable and shouldn't be hard-coded.  
   To use a variable for the port, app.listen should be set to a variable, server.
   server.address() is an object, so if it's used in the console.log as a variable, the entire object will be in the console.log statement. 
   server.address = {
@@ -32,8 +83,10 @@ app.use(express.static(__dirname));
     family: "IPv6",
     port: 3000
   }.
-  To just get the port, use: server.address().port in the console.log. */ 
-var server = app.listen(3000, () => {
+  To see what port the app is using, console.log: server.address().port in the console.log. 
+  Because socket.io is being used in this project, app.listen is being replaced with http.listen. 
+  When socket.io was added, an http server was required, changing what specific component is doing the listening. */ 
+var server = http.listen(3000, () => {
     console.log("The server is up and running on port: ", server.address().port);
 });
 
